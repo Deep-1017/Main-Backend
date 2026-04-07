7b570424161f4ac15420f276162be30d05e14273
DeepKansara1212 <deepkansara1212@gmail.com>
2026-04-07 13:49:38 +0530

    Add Person Model

--- 03_Hotel_Example/index.js
+++ 03_Hotel_Example/index.js
@@ -4,6 +4,36 @@ const PORT = 3000;
 
 const db = require("./db");
 
+const Person = require("./models/Person.model");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.post("/add-person", async (req, res) => {
+  try {
+    const data = req.body;
+    const newPerson = new Person(data);
+
+    const savedPerson = await newPerson.save();
+
+    console.log("Saved person to database");
+    res.status(201).json(savedPerson);
+  } catch (error) {
+    console.error("Error saving Person: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/get-persons", async (req, res) => {
+  try {
+    const persons = await Person.find();
+    res.status(200).json(persons);
+  } catch(error) {
+    console.error("Error fetching Person: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+})
+
 app.get("/", (req, res) => {
   res.send("Welcome to my Restaurent !");
 });
@@ -20,7 +50,7 @@ app.get("/menu/idli", (req, res) => {
     is_chutney: false,
   };
 
-  res.send(customized_idli.size);
+  res.send(customized_idli);
 });
 
 app.listen(PORT, () => {
---
+++ 03_Hotel_Example/models/Person.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+// Create a Person Schema
+const PersonSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  work: {
+    type: String,
+    enum: ["chef", "owner", "manager", "waiter"],
+    required: true
+  },
+  mobile: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  address: {
+    type: String,
+  },
+  salary: {
+    type: Number,
+    required: true
+  },
+});
+
+// Creating a Model
+const Person = mongoose.model("Person", PersonSchema);
+
+module.exports = Person;
